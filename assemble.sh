6a4d92907e0cf0bd90e2bfd136bb0d26086c3275
DIVV <diegod7_1090@outlook.com>
2019-01-27 19:24:37 -0600

    Se agregan permisos chmod 775 a los ficheros .sh

--- assemble.sh
+++ assemble.sh
@@ -3,4 +3,3 @@ mvn clean compile
 
 #Maven sin path
 #/home/gvalencia/Descargas/apache-maven-3.6.0/bin/mvn clean compile
-
--- avengers.sh
+++ avengers.sh
@@ -6,4 +6,3 @@ mvn spring-boot:run
 
 #Maven sin path
 #/home/gvalencia/Descargas/apache-maven-3.6.0/bin/mvn spring-boot:run
-
